9c4bde3a220c7685fd856574da342e5792b696e5
Oskar Ek <oskarek24@gmail.com>
2016-10-30 19:41:19 +0100

    added main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 /**
+ * Main class of the program.
  * Created by oskarek on 2016-10-30.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
 }
