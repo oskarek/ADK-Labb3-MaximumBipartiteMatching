ba4c334160456ecf9211e17ee20d66a282e6f884
Oskar Ek <oskarek24@gmail.com>
2016-11-03 14:50:29 +0100

    Implemented the printing of the final graph

--- src/FlowSolver.java
+++ src/FlowSolver.java
@@ -1,17 +1,25 @@
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by oskarek on 2016-11-02.
  */
 public class FlowSolver {
+    Kattio io;
+
     public static void main(String[] args) {
         new FlowSolver();
     }
 
     FlowSolver() {
+        io = new Kattio(System.in, System.out);
+
         GraphType graph = readCapacityGraph();
+
         GraphType maxGraph = edmonds_Karp(graph);
 
+        writeFlowGraph(maxGraph);
     }
 
     GraphType edmonds_Karp(GraphType graph) {
@@ -60,7 +68,7 @@ public class FlowSolver {
      * @return A list with the vertices in the path, beginning with start,
      *         or Optinal.empty() if no such path exists.
      */
-    Optional<LinkedList<GraphEdgeType>> BFS_path(GraphType graph, int from, int to) {
+    private Optional<LinkedList<GraphEdgeType>> BFS_path(GraphType graph, int from, int to) {
         Queue<LinkedList<GraphEdgeType>> queue = new LinkedList<>();
         LinkedList<GraphEdgeType> l = new LinkedList<>();
         l.addAll(graph.edgesForVertex(from));
@@ -81,18 +89,60 @@ public class FlowSolver {
         return Optional.empty();
     }
 
-    GraphType readCapacityGraph(){
-        Kattio io = new Kattio(System.in, System.out);
+    private GraphType readCapacityGraph() {
         int v = io.getInt();
         int s = io.getInt();
         int t = io.getInt();
         int e = io.getInt();
-        Graph graph = new Graph(v);
+        GraphType graph;
         for(int i = 0; i<e; i++){
             int a = io.getInt();
             int b = io.getInt();
             int f = io.getInt();
+            graph.addEdge(a,b,f);
         }
-        return null;
+        return graph;
+    }
+
+    private void writeFlowGraph(GraphType graph) {
+        int vertexCount = graph.vertexCount();
+        io.println(vertexCount);
+        int s = graph.getS(), t = graph.getT();
+        int totFlow = 0;
+        for (GraphEdgeType sEdge : graph.edgesForVertex(s))
+            totFlow += sEdge.value();
+        io.write(s + " " + t + " " + totFlow);
+
+        int positiveEdgesCount = 0;
+        LinkedList<GraphEdgeType> positiveEdges = new LinkedList<>();
+        for (GraphEdgeType edge : graph.edges()) {
+            if (edge.value() > 0) {
+                positiveEdgesCount += 1;
+                positiveEdges.add(edge);
+            }
+        }
+
+        io.println(positiveEdgesCount);
+
+        for (GraphEdgeType e : positiveEdges)
+            io.println(e.getFrom() + " " + e.getTo() + " " + e.value());
+    }
+
+    private void writeFlowGraph_ROBERT_HATAR(GraphType graph) {
+        int vertexCount = graph.vertexCount();
+        io.println(vertexCount);
+        int s = graph.getS(), t = graph.getT();
+        int totFlow = graph.edgesForVertex(s).stream()
+                .mapToInt(GraphEdgeType::value)
+                .sum();
+        io.write(s + " " + t + " " + totFlow);
+
+        Stream<GraphEdgeType> positiveFlowEdges = graph.edges().stream()
+                .filter(e -> e.value() > 0);
+
+        io.println(positiveFlowEdges.count());
+
+        positiveFlowEdges
+                .forEach(e -> io.println(e.getFrom() + " " + e.getTo() + " " + e.value()));
     }
 }
